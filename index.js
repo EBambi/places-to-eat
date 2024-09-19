461a10f771287c70932a041672ece8511c1ebba9
Esteban <estebanyalg@gmail.com>
2024-09-19 18:59:41 -0500

    Add basic POST structure

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+app.use(express.json())
 
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 )
+
+app.post('/places/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { name } = req.body;
+    const { rate } = req.body;
+
+    if (!rate | !name) {
+        res.status(418).send({ message: 'Values are missing'})
+    }
+
+    res.send({
+        place: `Place ${name} has been created with a ${rate} rate`
+    })
+
+});
+
+app.get('/places/:id', (req, res) => {
+    res.status(200).send({})
+    res.send({
+        place: `${id.name} has a ${id.rate} rate`
+    })
+})
